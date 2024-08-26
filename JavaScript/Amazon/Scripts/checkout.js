09d188ce8f9ad6a0820c895fb2df705951e5dbfc
Pratik Raj <pratikraj220011@gmail.com>
2024-08-26 14:55:38 +0530

    backend pratice

--- JavaScript/Amazon/Scripts/checkout.js
+++ JavaScript/Amazon/Scripts/checkout.js
@@ -1,6 +1,7 @@
 import {render} from './checkout/orderSummary.js';
 import {paymentRender} from './checkout/paymentSummay.js';
 //import '../data/cart-class.js';
+import '../data/backend.js'
 
 render();
 paymentRender();
