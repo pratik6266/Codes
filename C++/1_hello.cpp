8e201b90c3391269fb922b5f7f3ff5d8677a9306
Pratik Raj <pratikraj220011@gmail.com>
2024-08-27 23:53:04 +0530

    learning cpp

--- C++/1_hello.cpp
+++ C++/1_hello.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class employee
+{
+    private:   // can't be acces outside so use method
+        int a, b, c;
+
+    public:  // can be accessed any where 
+        int d, e;
+
+    void setdata(int a, int b, int c);
+    void getdata(){
+        cout<<a<<endl;
+        cout<<b<<endl;
+        cout<<c<<endl;
+        cout<<d<<endl;
+        cout<<e<<endl;
+    }
+};
+
+void employee::setdata(int a1, int b1, int c1)
+{
+    a = a1;
+    b = b1;
+    c = c1;
+}
+
 int main()
 {
-    cout<<"Hello Word";
+    employee harry;
+    harry.setdata(1,2,3);
+    harry.d = 4;
+    harry.e = 5;
+    harry.getdata();
     return 0;
 }
