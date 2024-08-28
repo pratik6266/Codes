cc756c4e066cdf02af881f500f744ace526fb3d5
Pratik Raj <pratikraj220011@gmail.com>
2024-08-29 00:57:19 +0530

    just learnign cpp

--- C++/1_hello.cpp
+++ C++/1_hello.cpp
@@ -1,43 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class complex
-{
-    int real;
-    int img;
-
-    public: 
-
-        friend void sum(complex a, complex b); // private or public don't matter
-
-        void set()
-        {
-            cout<<"Enter real part: ";
-            cin>>real;
-            cout<<"Enter imaginary part: ";
-            cin>>img;
-        }
-
-        void display()
-        {
-            cout<<real<<" + "<<img<<"i"<<endl;
-        }       
-};
-
-void sum(complex a, complex b)
-{
-    int x = a.real + b.real;
-    int y = a.img + b.img;
-    cout<<x<<" + "<<y<<"i"<<endl;
-}
-
 int main()
 {
-    complex a ,b ,c;
-    a.set();
-    a.display();
-    b.set();
-    b.display();
-    sum(a,b);
+    
     return 0;
 }
